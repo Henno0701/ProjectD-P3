485dd3719c0e7e12e22c5dc22666a32873b9cd72
Henno0701 <henno.passchier@gmail.com>
2024-03-21 18:31:22 +0100

    build change

--- prototype3/src/components/ExploreContainer.tsx
+++ prototype3/src/components/ExploreContainer.tsx
@@ -1,3 +1,4 @@
+import { IonAlert, IonButton } from '@ionic/react';
 import './ExploreContainer.css';
 
 interface ContainerProps { }
@@ -7,6 +8,14 @@ const ExploreContainer: React.FC<ContainerProps> = () => {
     <div id="container">
       <strong>Ready to create an app?</strong>
       <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <IonButton id='present-alert'>Show Alert</IonButton>
+      <IonAlert
+        trigger='present-alert'
+        header="A Short Title Is Best"
+        subHeader="A Sub Header Is Optional"
+        message="A message should be a short, complete sentence."
+        buttons={['Action']}
+        ></IonAlert>
     </div>
   );
 };
